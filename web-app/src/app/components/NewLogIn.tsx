2a3db31526eb9d9338d943a09527acd3fad55f38
Sama Tarraf <samatarraf1280@gmail.com>
2025-01-31 09:52:05 -0500

    login is working

---
+++ web-app/src/app/components/NewLogIn.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React, {useState, useEffect, useRef} from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+export default function NewLogIn() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const checkFields = async() => {
+            if(username.trim().length===0 || username.length===0 || password.trim().length===0 || password.length===0){
+                console.log("a feild is empty")
+                setError("Fill in both username and password fields");
+                return;
+            }
+
+            const response = await axios.post('http://localhost:5000/logIn' , {user: username, password: password});
+
+            if(!response.data.isUsernameExisting){
+                setError("Username doesnt exist")
+            }
+            else if(!response.data.isPasswordVerified){
+                setError("Password is not valid")
+            }
+            else{
+                router.push('/dashboard');
+            }
+
+
+        
+
+    }
+
+    return(
+        <div className="flex flex-col justify-center w-3/4 h-screen bg-slate-400 absolute left-1/2 -translate-x-1/2">
+            <input type='text' onChange={(e)=>setUsername(e.target.value)} value={username}/>
+
+            <input type='text' onChange={(e)=>setPassword(e.target.value)} value={password}/>
+
+            <button onClick={checkFields}>Log In</button>
+
+            <div>{error}</div>
+
+        </div>
+    )
+}
